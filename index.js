448cbb1aa324a01ae54568ba94022a3dad64023a
Tomi-ar <tomarrillaga@gmail.com>
2022-08-30 19:03:20 -0300

    cambiando posicion del static

--- index.js
+++ index.js
@@ -13,24 +13,24 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// MOTOR DE PLANTILLAS
+// app.set("view engine", "ejs");
+// app.set("views", __dirname + "/views");
+app.use(express.static(__dirname+"/satsuma-app/build"));
+
 // ROUTER
 const materiales = require("./src/routes/materiales");
 app.use("/materiales", materiales);
 const usuarios = require("./src/routes/users");
 app.use("/login", usuarios);
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-// MOTOR DE PLANTILLAS
-// app.set("view engine", "ejs");
-// app.set("views", __dirname + "/views");
-app.use(express.static(__dirname+"/satsuma-app/build"));
 app.get('*', (req,res) => {
   res.sendFile(__dirname+'/satsuma-app/build/'+'index.html')
 })
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 app.listen(process.env.PORT || 8002 , () => {
   console.log(`Escuchando en el puerto ${process.env.PORT}`);
